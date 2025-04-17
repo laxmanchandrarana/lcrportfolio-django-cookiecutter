96f98bc2df70f930f4ee4faa9ed80c5db7fcc2ff
LAXMAN CHANDRA RANA <laxmanchandrarana2000@gmail.com>
2025-04-18 05:28:30 +0530

    first awesome commit

---
+++ lcrportfolio_slug/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from lcrportfolio_slug.users.models import User
+from lcrportfolio_slug.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
